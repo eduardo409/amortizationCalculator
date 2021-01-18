4e54719d32fe0a4422cda33c92422f0200d90064
eduardo Hernandez <eduardo_hernandez@homedepot.com>
2021-01-17 23:15:52 -0500

    added firebase

--- src/firebase/db.js
+++ src/firebase/db.js
@@ -1,51 +1,15 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
-/* eslint no-console:  */
 
-// Imports the Secret Manager library
-const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
-
-
-// Instantiates a client
-const client = new SecretManagerServiceClient();
-
-async function accessSecret(secret) {
-  const name = `projects/amortization-calculator-266503/secrets/${secret}/versions/latest`;
-  const [version] = await client.accessSecretVersion({
-    name: name,
-  });
-
-  // Extract the payload as a string.
-  const payload = version.payload.data.toString();
-  console.log("payload: " +payload)
-  return payload
-}
-async function setup() {
-  var apiKey = await accessSecret("apiKey")
-  var authDomain = await accessSecret("authDomain")
-  var projectId = await accessSecret("projectId")
-  var storageBucket = await accessSecret("storageBucket")
-  var messagingSenderId = await accessSecret("messagingSenderId")
-  var appId = await accessSecret("appId")
-  var measurementId = await accessSecret("measurementId")
-
-  // Get a Firestore instance
-  firebaseConfig = {
-    apiKey: apiKey,
-    authDomain: authDomain,
-    projectId: projectId,
-    storageBucket: storageBucket,
-    messagingSenderId: messagingSenderId,
-    appId: appId,
-    measurementId: measurementId
-  };
-  return firebaseConfig
-}
-
-var firebaseConfig
-setup().then(response =>{
-  firebaseConfig = response
-})
+const firebaseConfig = {
+  apiKey: "AIzaSyAThy0SMJDPT-FtgW5YLSdN7EP2t64TNvA",
+  authDomain: "amortization-calculator-266503.firebaseapp.com",
+  projectId: "amortization-calculator-266503",
+  storageBucket: "amortization-calculator-266503.appspot.com",
+  messagingSenderId: "597617018945",
+  appId: "1:597617018945:web:6b39bd97746b8b23229ec0",
+  measurementId: "G-1ZY2ZL2NF5"
+};
 export const db = firebase.initializeApp(firebaseConfig).firestore();
 
 // Export types that exists in Firestore
